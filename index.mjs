37989baf1c72b9ea4a58c7629372809784b32de3
Nilesh Gautam <nileshgautam13092001+git@gmail.com>
2024-03-17 14:39:42 +0530

    Get Requests

--- index.mjs
+++ index.mjs
@@ -2,6 +2,24 @@ import express from "express";
 
 const app = express();
 const PORT = 3000;
+const USERS = [
+    { id : "1", username : "nilesh", displayName : "Nilesh Gautam"},
+    { id : "2", username : "aman", displayName : "Aman Kumar"},
+    { id : "3", username : "ankur", displayName : "Ankur Gautam"},
+    { id : "4", username : "manish", displayName : "Manish Kumar"},
+]
+
+app.get("/", (req, res)=>{
+    res.status(201).send(USERS[0]);
+})
+
+app.get("/api/users", (req, res)=>{
+    res.status(200).send(USERS);
+})
+
+app.get("/api/products", (req, res)=>{
+    res.status(200).send({ id : "1", name : "Chicken Breast", price : "10.99"});
+})
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port : ${PORT}`);
