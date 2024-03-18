2fd6157e5c972375bdc2d986f00dd6a2853e6c97
Nilesh Gautam <nileshgautam13092001+git@gmail.com>
2024-03-19 01:00:16 +0530

    Query Parameters

--- index.mjs
+++ index.mjs
@@ -7,6 +7,7 @@ const USERS = [
     { id : 2, username : "aman", displayName : "Aman Kumar"},
     { id : 3, username : "ankur", displayName : "Ankur Gautam"},
     { id : 4, username : "manish", displayName : "Manish Kumar"},
+    { id : 5, username : "sandeep", displayName : "Sandeep Bera"},
 ];
 
 app.get("/", (req, res)=>{
@@ -14,7 +15,13 @@ app.get("/", (req, res)=>{
 });
 
 app.get("/api/users", (req, res)=>{
-    res.status(200).send(USERS);
+    console.log(req.query);
+    const {query : {filter, value}} = req;
+    if(!filter && !value) return req.status(200).send(USERS);
+    if(filter && value) return res.status(200).send(
+        USERS.filter((user) => user[filter].includes(value))
+    )
+    res.status(400).send({Error : "Bad Request!"});
 });
 
 app.get("/api/users/:id", (req, res)=>{
