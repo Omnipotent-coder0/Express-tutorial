4fa3b757bdccb6dd34959c28f5d11ff012ff8945
Nilesh Gautam <nileshgautam13092001+git@gmail.com>
2024-03-19 22:05:23 +0530

    Cookies and signed cookies

--- src/index.mjs
+++ src/index.mjs
@@ -1,15 +1,18 @@
 import express from "express";
 import { USERS } from "./utils/constants.mjs";
 import routes from "./routes/index.mjs"
+import cookieParser from "cookie-parser";
 
 const PORT = 3000;
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser("secret"))
 app.use(routes);
 
 app.get("/", (req, res) => {
+  res.cookie("hello", "world", { maxAge : 60000 * 60, signed : true});
   res.status(201).send(USERS[0]);
 });
 
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -3,7 +3,15 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/api/products", (req, res) => {
-  res.status(200).send({ id: "1", name: "Chicken Breast", price: "10.99" });
+  console.log(req.headers.cookie);
+  console.log(req.cookies);
+  console.log(req.signedCookies);
+  console.log(req.signedCookies.hello);
+  if (req.signedCookies.hello && req.signedCookies.hello === "world")
+    return res
+      .status(200)
+      .send({ id: "1", name: "Chicken Breast", price: "10.99" });
+  return res.status(401).send({ error : "You are not Authorized!"});
 });
 
 export default router;
